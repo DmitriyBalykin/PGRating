d3e136b58ec0b66c32b2cf781725781cf689dfe2
Dmytro Balykin <dmitriy.a.balykin@gmail.com>
2017-09-09 21:16:27 +0300

    Initial commit - add application

---
+++ PGRating/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(PGRating.Startup))]
+
+namespace PGRating
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
